7f0a557ad81342c8ee8c8408a6b00c2325e97230
Tord Sætermo <tord.saetermo@users.noreply.github.com>
2024-03-14 12:22:09 +0100

    feat: add radial speed variance and expectation value

--- lab-4/script/main.py
+++ lab-4/script/main.py
@@ -1,8 +1,10 @@
+from scipy.signal import hann
+import numpy as np
+
 from raspi_import import raspi_import
 from plot import read_csv_with_header, bode_plot, spectrum_plot, time_plot
 from fft import calc_spectrum
 
-from scipy.signal import hann
 
 missing_file_path_error = (
     "\nMissing file path\n\nUsage example: python main.py foo.bin\n"
@@ -13,7 +15,7 @@ V_REF = 3.3
 DC_OFFSET = 1
 
 
-def main():
+def find_radial_speed(file_path):
     #
     # import adc data and remove dc component
     #
@@ -26,23 +28,44 @@ def main():
         data, sample_period, title="Time plot of data", save_plot=True, show_plot=True
     )
 
-    #
-    # example: caluculate and plot power spectrum with and wihtout hann window
-    #
+    I = data[row][com]
+    Q = data[row][com + 1]
+
+    complex_data = I + 1j * Q
+
     lower_bounds = 0
     upper_bounds = -1
-    data_slice = data[lower_bounds:upper_bounds]
+    data_slice = complex_data[lower_bounds:upper_bounds]
 
     NFFT = 524288
 
     data_spec, _ = calc_spectrum(data_slice, NFFT)
-    spectrum_plot(data_spec, sample_period, save_plot=False, show_plot=True)
+    spectrum_plot(data_spec, sample_period, "spectrum", save_plot=False, show_plot=True)
 
     data_windowed = data_slice * hann(len(data_slice))
 
     data_spec, _ = calc_spectrum(data_windowed, NFFT)
-    spectrum_plot(data_spec, sample_period, save_plot=False, show_plot=True)
+    spectrum_plot(
+        data_spec,
+        sample_period,
+        "spectrum with Hanning window",
+        save_plot=False,
+        show_plot=True,
+    )
+
+    f0 = 24.13 * 10**9  # center frequency = 24,13 [GHz]
+    c = 299792458  # speed of light [m/s]
+    fD = np.argmax(data_windowed)  # Doppler frequency shift [Hz]
+    v_r = fD * c / (2 * f0)  # radial speed [m/s]
+
+    return v_r
 
 
 if __name__ == "__main__":
-    main()
+    files = ["1.bin", "2.bin", "3.bin", "4.bin", "5.bin"]
+    radial_speeds = []
+    for file in files:
+        radial_speed = find_radial_speed(file)
+        radial_speeds.append(radial_speed)
+    my = np.mean(radial_speeds)
+    std = np.std(radial_speeds)
